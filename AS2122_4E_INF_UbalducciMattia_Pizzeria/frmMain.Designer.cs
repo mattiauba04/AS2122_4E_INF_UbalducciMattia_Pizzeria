9eba45a7b6d9d2a13a333361da0223c7353112a4
uba <mattiaubalducci6@gmail.com>
2022-03-09 09:35:56 +0100

    gestione pizzeria,creazione interfaccia

--- AS2122_4E_INF_UbalducciMattia_Pizzeria/frmMain.Designer.cs
+++ AS2122_4E_INF_UbalducciMattia_Pizzeria/frmMain.Designer.cs
@@ -29,20 +29,142 @@ namespace AS2122_4E_INF_UbalducciMattia_Pizzeria
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotaleVerdura = new System.Windows.Forms.Label();
+            this.lblPizzeMargherita = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstVisualizza = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblTotaleVerdura
+            // 
+            this.lblTotaleVerdura.AutoSize = true;
+            this.lblTotaleVerdura.Location = new System.Drawing.Point(535, 299);
+            this.lblTotaleVerdura.Name = "lblTotaleVerdura";
+            this.lblTotaleVerdura.Size = new System.Drawing.Size(38, 15);
+            this.lblTotaleVerdura.TabIndex = 3;
+            this.lblTotaleVerdura.Text = "label3";
+            this.lblTotaleVerdura.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblPizzeMargherita
+            // 
+            this.lblPizzeMargherita.AutoSize = true;
+            this.lblPizzeMargherita.Location = new System.Drawing.Point(535, 252);
+            this.lblPizzeMargherita.Name = "lblPizzeMargherita";
+            this.lblPizzeMargherita.Size = new System.Drawing.Size(131, 15);
+            this.lblPizzeMargherita.TabIndex = 2;
+            this.lblPizzeMargherita.Text = "Totale pizze margherite:";
+            this.lblPizzeMargherita.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(535, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(535, 346);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 15);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "label7";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(171, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 39);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lstVisualizza
+            // 
+            this.lstVisualizza.FormattingEnabled = true;
+            this.lstVisualizza.ItemHeight = 15;
+            this.lstVisualizza.Location = new System.Drawing.Point(535, 80);
+            this.lstVisualizza.Name = "lstVisualizza";
+            this.lstVisualizza.Size = new System.Drawing.Size(120, 94);
+            this.lstVisualizza.TabIndex = 8;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstVisualizza);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTotaleVerdura);
+            this.Controls.Add(this.lblPizzeMargherita);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "frmMain";
             this.Text = "Ubalducci Mattia;lab2.2;4E;Gestione pizzeria";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTotaleVerdura;
+        private System.Windows.Forms.Label lblPizzeMargherita;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstVisualizza;
     }
 }
 
--- AS2122_4E_INF_UbalducciMattia_Pizzeria/frmMain.cs
+++ AS2122_4E_INF_UbalducciMattia_Pizzeria/frmMain.cs
@@ -17,5 +17,19 @@ namespace AS2122_4E_INF_UbalducciMattia_Pizzeria
             InitializeComponent();
         }
 
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
